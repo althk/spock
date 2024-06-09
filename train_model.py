1ae37952b5be88aae99fb4a35dd8e1c794506321
Harish Kotha <althk@users.noreply.github.com>
2024-06-09 19:06:52 +0530

    fix issues caused due to incorrect model instantiation

--- train_model.py
+++ train_model.py
@@ -15,7 +15,7 @@ flags.DEFINE_string('data_dir', None,
                     'Path to the directory that contains the training data.')
 
 
-def _load_and_train(model_inst, param_grid, data_dir, output_dir, model_name):
+def _load_and_train(model_name, data_dir, output_dir):
     data_files = os.listdir(data_dir)
     total = len(data_files)
     futures = []
@@ -24,18 +24,21 @@ def _load_and_train(model_inst, param_grid, data_dir, output_dir, model_name):
             file_path = os.path.join(data_dir, data_file)
             ticker = data_file.split('_')[0]
             logging.info(f'{i+1}/{total} processing {file_path}')
-            future = executor.submit(_train_and_save, model_inst, param_grid, file_path, ticker, output_dir, model_name)
+            future = executor.submit(_train_and_save, model_name, file_path, ticker, output_dir)
             futures.append(future)
-        concurrent.futures.wait(futures)
+    concurrent.futures.wait(futures)
 
 
-def _train_and_save(model_inst, param_grid, file_path, ticker, output_dir, model_name):
+def _train_and_save(model_name, file_path, ticker, output_dir):
     logging.info(f'{ticker}: loading data from {file_path}')
     raw_data = util.load_historical_data(file_path)
     scaler, X_scaled, y = util.preprocess_data(raw_data)
     X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2)
+    logging.info(f'{ticker}: X_train.shape = {X_train.shape}, X_test.shape = {X_test.shape}'
+                 f'y_train.shape = {y_train.shape}, y_test.shape = {y_test.shape}')
     logging.info(f'{ticker}: running grid search cv for best estimator')
-    estimator, _ = util.evaluate_tr_model(model_inst, param_grid, X_train, y_train, X_test, y_test)
+    model_factory, param_grid = util.get_model(model_name)
+    estimator, _ = util.evaluate_tr_model(model_factory, param_grid, X_train, y_train, X_test, y_test)
     _save_model(estimator, scaler, output_dir, model_name, ticker)
 
 
@@ -52,8 +55,7 @@ def main(_):
     logging.info(f'parsing results from {FLAGS.results_file}')
     _, best_model_name, _ = util.parse_evaluation_results(FLAGS.results_file)
     logging.info(f'Best model: {best_model_name}')
-    model_inst, param_grid = util.get_model(best_model_name)
-    _load_and_train(model_inst, param_grid, FLAGS.data_dir, output_dir)
+    _load_and_train(best_model_name, FLAGS.data_dir, output_dir)
 
 
 if __name__ == '__main__':
